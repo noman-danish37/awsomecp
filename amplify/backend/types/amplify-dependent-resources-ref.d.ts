97d0a9a75b26bcb00e38ce74aeab70903f079594
Noman danish <noman.danish2001@gmail.com>
2023-12-05 22:01:53 +0530

    cognito authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsomecomp1f48b288": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+// import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
 
-import './App.css';
+import { Authenticator, components } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+// import { signOut } from 'aws-amplify/auth';
+
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-        <h1>Welcome to amplify</h1>
-    </div>
+    <Authenticator components = { components }>
+      {({ signOut , user }) => (
+          <div className='App'>
+            <h1>Hello {user.username}</h1>
+            <button onClick={ signOut}>Sign out</button>
+           
+          </div>
+
+      )}
+    </Authenticator>
   );
 }
+
+
+
 
 export default App;
